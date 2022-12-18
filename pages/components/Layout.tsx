1deb63f92ee38a6ea64f5d7ca48287f1f4921b3b
Jaiane Oliveira <jaianeoliveira.dev@gmail.com>
2022-12-18 06:30:48 -0300

    feat: header

--- pages/components/Layout.tsx
+++ pages/components/Layout.tsx
@@ -1,3 +1,10 @@
+import {
+	Menu,
+	MenuHandler,
+	MenuItem,
+	MenuList,
+} from '@material-tailwind/react';
+import { SignOut, User } from 'phosphor-react';
 import { ReactNode } from 'react';
 import { w } from 'windstitch';
 import Sidebar from './Sidebar';
@@ -10,11 +17,78 @@ const Container = w.div(`
     flex bg-neutral-800 w-full h-[100vh] text-neutral-100 
 `);
 
+const Header = w.header(`
+	h-[8vh]
+	w-full
+	px-[6vw]
+	py-8
+	flex
+	items-center
+	justify-end
+	gap-4
+	text-sm
+`);
+
+const Content = w.div(`
+	pr-[6vw]
+	pl-[2vw]
+	py-4
+`);
+
+const Main = w.main(`
+	w-full
+	h-full
+`);
+
+const Avatar = w.div(
+	`
+	bg-neutral-600
+	w-8
+	h-8
+	rounded-full
+	shadow-md
+	flex
+	items-center
+	justify-center
+	text-neutral-100
+	cursor-pointer
+`,
+	{
+		defaultProps: {
+			children: <User size={18} />,
+		},
+	}
+);
+
 const Layout = ({ children }: LayoutProps) => {
 	return (
 		<Container>
 			<Sidebar />
-			{children}
+			<Main>
+				<Header>
+					Jaiane Oliveira
+					<Menu placement="bottom-end">
+						<MenuHandler>
+							<Avatar />
+						</MenuHandler>
+						<MenuList>
+							<MenuItem>
+								<span className="flex gap-2">
+									<User size={20} weight="bold" />
+									Ver perfil
+								</span>
+							</MenuItem>
+							<MenuItem>
+								<span className="flex gap-2">
+									<SignOut size={20} weight="bold" />
+									Sair
+								</span>
+							</MenuItem>
+						</MenuList>
+					</Menu>
+				</Header>
+				<Content>{children}</Content>
+			</Main>
 		</Container>
 	);
 };
